00e69991318e25d2e02fef3c398f892ad2784682
sunyang401769@126.com <superdog333>
2019-07-31 16:34:18 +0800

    菜单

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -59,103 +59,103 @@ const actions = {
         //console.log('>>>>>>>   ' + JSON.stringify(asyncRoutes))
         accessedRoutes = asyncRoutes || []
       } else {
-        //accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
+        accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
         /**
          * 动态组装路由
          */
           ///let aaa = state.user.userMenuList
-        let userMenuList = rootState.user.userMenuList
-        console.log('userMenuList', userMenuList)
-        let accessedRoutesNew = []
-        userMenuList.forEach(userMenu => {
-          let accessedRoutesItem = {}
-          accessedRoutesItem.path = userMenu.path
-          accessedRoutesItem.alwaysShow = true
-          accessedRoutesItem.component = () => import('@/layout')
-          let children = userMenu.children
-          accessedRoutesNew.push(accessedRoutesItem)
-        })
-        console.log('accessedRoutesNew', accessedRoutesNew)
-
-        accessedRoutes = [
-          {
-            path: '/tab',
-            hidden: true,
-            component: () => import('@/layout'),
-            children: [
-              {
-                path: 'index',
-                component: () => import('@/views/tab/index'),
-                name: 'Tab',
-                meta: { title: 'Tab', icon: 'tab' }
-              }
-            ]
-          },
-
-          {
-            path: '/error',
-            component: () => import('@/layout'),
-            alwaysShow: true,
-            // redirect: 'noRedirect',
-            name: 'ErrorPages',
-            meta: {
-              title: 'Error Pages',
-              icon: '404',
-              roles: ['admin']
-            },
-            children: [
-              {
-                path: '401',
-                hidden: false,
-                component: () => import('@/views/error-page/401'),
-                name: 'Page401',
-                meta: { title: '401', noCache: true }
-              },
-              {
-                path: '404',
-                hidden: false,
-                component: () => import('@/views/error-page/404'),
-                name: 'Page404',
-                meta: { title: '404', noCache: true, roles: ['admin111'] }
-              }
-            ]
-          }, {
-            path: '/excel',
-            component: () => import('@/layout'),
-            redirect: '/excel/export-excel',
-            name: 'Excel',
-            meta: {
-              title: 'Excel',
-              icon: 'excel'
-            },
-            children: [
-              {
-                path: 'export-excel',
-                component: () => import('@/views/excel/export-excel'),
-                name: 'ExportExcel',
-                meta: { title: 'Export Excel' }
-              },
-              {
-                path: 'export-selected-excel',
-                component: () => import('@/views/excel/select-excel'),
-                name: 'SelectExcel',
-                meta: { title: 'Export Selected' }
-              },
-              {
-                path: 'export-merge-header',
-                component: () => import('@/views/excel/merge-header'),
-                name: 'MergeHeader',
-                meta: { title: 'Merge Header' }
-              },
-              {
-                path: 'upload-excel',
-                component: () => import('@/views/excel/upload-excel'),
-                name: 'UploadExcel',
-                meta: { title: 'Upload Excel' }
-              }
-            ]
-          }
-        ]
+        // let userMenuList = rootState.user.userMenuList
+        // console.log('userMenuList', userMenuList)
+        // let accessedRoutesNew = []
+        // userMenuList.forEach(userMenu => {
+        //   let accessedRoutesItem = {}
+        //   accessedRoutesItem.path = userMenu.path
+        //   accessedRoutesItem.alwaysShow = true
+        //   accessedRoutesItem.component = () => import('@/layout')
+        //   let children = userMenu.children
+        //   accessedRoutesNew.push(accessedRoutesItem)
+        // })
+        // console.log('accessedRoutesNew', accessedRoutesNew)
+        //
+        // accessedRoutes = [
+        //   {
+        //     path: '/tab',
+        //     hidden: true,
+        //     component: () => import('@/layout'),
+        //     children: [
+        //       {
+        //         path: 'index',
+        //         component: () => import('@/views/tab/index'),
+        //         name: 'Tab',
+        //         meta: { title: 'Tab', icon: 'tab' }
+        //       }
+        //     ]
+        //   },
+        //
+        //   {
+        //     path: '/error',
+        //     component: () => import('@/layout'),
+        //     alwaysShow: true,
+        //     // redirect: 'noRedirect',
+        //     name: 'ErrorPages',
+        //     meta: {
+        //       title: 'Error Pages',
+        //       icon: '404',
+        //       roles: ['admin']
+        //     },
+        //     children: [
+        //       {
+        //         path: '401',
+        //         hidden: false,
+        //         component: () => import('@/views/error-page/401'),
+        //         name: 'Page401',
+        //         meta: { title: '401', noCache: true }
+        //       },
+        //       {
+        //         path: '404',
+        //         hidden: false,
+        //         component: () => import('@/views/error-page/404'),
+        //         name: 'Page404',
+        //         meta: { title: '404', noCache: true, roles: ['admin111'] }
+        //       }
+        //     ]
+        //   }, {
+        //     path: '/excel',
+        //     component: () => import('@/layout'),
+        //     redirect: '/excel/export-excel',
+        //     name: 'Excel',
+        //     meta: {
+        //       title: 'Excel',
+        //       icon: 'excel'
+        //     },
+        //     children: [
+        //       {
+        //         path: 'export-excel',
+        //         component: () => import('@/views/excel/export-excel'),
+        //         name: 'ExportExcel',
+        //         meta: { title: 'Export Excel' }
+        //       },
+        //       {
+        //         path: 'export-selected-excel',
+        //         component: () => import('@/views/excel/select-excel'),
+        //         name: 'SelectExcel',
+        //         meta: { title: 'Export Selected' }
+        //       },
+        //       {
+        //         path: 'export-merge-header',
+        //         component: () => import('@/views/excel/merge-header'),
+        //         name: 'MergeHeader',
+        //         meta: { title: 'Merge Header' }
+        //       },
+        //       {
+        //         path: 'upload-excel',
+        //         component: () => import('@/views/excel/upload-excel'),
+        //         name: 'UploadExcel',
+        //         meta: { title: 'Upload Excel' }
+        //       }
+        //     ]
+        //   }
+        // ]
       }
       commit('SET_ROUTES', accessedRoutes)
       resolve(accessedRoutes)
